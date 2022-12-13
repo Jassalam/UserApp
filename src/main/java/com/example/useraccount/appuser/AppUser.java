327bf8c4394a8aba57d75543aa60b25856b83c37
jasveerkaur <jasveerk1980@gmail.com>
2022-12-13 16:25:45 +0100

    updated work

--- src/main/java/com/example/useraccount/appuser/AppUser.java
+++ src/main/java/com/example/useraccount/appuser/AppUser.java
@@ -32,29 +32,26 @@ public class AppUser implements UserDetails {
     )
 
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
-    public AppUser(String name,
-                   String username,
+    public AppUser(String firstName,
+                   String lastName,
                    String email,
                    String password,
-                   AppUserRole appUserRole,
-                   Boolean locked,
-                   Boolean enabled) {
-        this.name = name;
-        this.username = username;
+                   AppUserRole appUserRole
+                  ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
@@ -66,14 +63,20 @@ public class AppUser implements UserDetails {
 
     @Override
     public String getPassword() {
-
         return password;
     }
 
     @Override
     public String getUsername() {
+        return email;
+    }
 
-        return username;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     @Override
